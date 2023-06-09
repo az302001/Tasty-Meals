32a0ca898ef118adfd99aa2884bb0370b9fffe88
lulilcm <lourdescandelamoreno@gmail.com>
2023-06-08 21:26:41 -0300

    ShoppingCart Finish

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,94 +1,5 @@
-// import React from 'react';
-// import Link from 'next/link';
-
-
-// import { PlusCircleIcon } from "@heroicons/react/24/outline";
-// import { useRecoilState } from 'recoil';
-// import { cartState } from '../../../atoms/cartState';
-// import { toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import Image from "next/image";
-// import Carrito from "@/assets/carro-de-la-carretilla.png";
-
-
-
-// const Card = ({ food }) => {
-//     const { id, name, image, price, description } = food;
-
-
-//     const [cartItem, setCartItem] = useRecoilState(cartState);
-
-//     const handleClick = () => {
-//         toast.success('Añadido al carrito!', {
-//             position: "bottom-center",
-//             autoClose: 1200,
-//             hideProgressBar: false,
-//             closeOnClick: true,
-//             pauseOnHover: true,
-//             draggable: true,
-//             progress: undefined,
-//             theme: "light",
-//         });
-//     };
-
-//     const addItemToCart = () => {
-//         if (cartItem.findIndex((fo) => fo.id === food.id) === -1) {
-//             setCartItem((prevState) => [...prevState, food]);
-//         } else {
-//             setCartItem((prevState) => {
-//                 return prevState.map((item) => {
-//                     return item.id === food.id ? { ...item, quantity: item.quantity + 1 } : item
-//                 })
-//             });
-//         }
-//         handleClick();
-//     };
-
-//     return (
-//         <div className="bg-color3 flex flex-col w-full items-center rounded-3xl">
-//             <div>
-//                 <Link href={`/detail?id=${id}`}>
-//                     <h1 className="font-pacifico text-3xl text-color1 p-1">{name}</h1>
-//                 </Link>
-//             </div>
-
-//             <div className='flex flex-row w-full rounded-3x1 px-4 justify-around'>
-
-//                 <div className="w-60 h-32">
-//                     <img src={image} alt={name} className="w-full h-full object-cover rounded-3xl" />
-//                 </div>
-
-
-
-//                 <div className='flex flex-col items-end justify-between'>
-//                     <div name="carrito-compras" className="w-9 h-9">
-//                         <Image src={Carrito} alt={"carrito"}></Image>
-//                     </div>
-
-
-//                 </div>
-
-//                 <p className="left-45.5 right-11.99 top-35 bottom-18 text-color2 font-josefin font-normal text-sm leading-150">{description}</p>
-
-//             </div>
-
-//             <p className="left-80 mt-12 font-pacifico font-normal text-20 leading-150">${price}</p>
-//             <div onClick={addItemToCart} className='cursor-pointer'>
-
-//                 <PlusCircleIcon className="h-10 w-10 bg-#E96479 inset-13 top-13 left-89.92 right-4.9 bottom-68 text-color2" />
-//             </div>
-
-//         </div>
-//     );
-// };
-
-// export default Card;
-
-
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-
-
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import { useRecoilState } from 'recoil';
 import { cartState } from '../../../atoms/cartState';
@@ -98,65 +9,94 @@ import Image from "next/image";
 import Carrito from "@/assets/carro-de-la-carretilla.png";
 
 const Card = ({ food }) => {
-    const { id, name, image, price, description } = food;
+  const { id, name, image, price, description } = food;
+  const [quantity, setQuantity] = useState(1);
+  const [cartItem, setCartItem] = useRecoilState(cartState);
 
+  const handleClick = () => {
+    toast.success('Añadido al carrito!', {
+      position: "bottom-center",
+      autoClose: 350,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
 
-    const [cartItem, setCartItem] = useRecoilState(cartState);
+  const handleIncrement = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
 
-    const handleClick = () => {
-        toast.success('Añadido al carrito!', {
-            position: "bottom-center",
-            autoClose: 1200,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      setQuantity((prevQuantity) => prevQuantity - 1);
+    }
+  };
+
+  const addItemToCart = () => {
+    const newItem = { ...food, quantity };
+    if (cartItem.findIndex((fo) => fo.id === food.id) === -1) {
+      setCartItem((prevState) => [...prevState, newItem]);
+    } else {
+      setCartItem((prevState) => {
+        return prevState.map((item) => {
+          return item.id === food.id ? { ...item, quantity: item.quantity + quantity } : item;
         });
-    };
+      });
+    }
+    handleClick();
+  };
 
-    const addItemToCart = () => {
-        if (cartItem.findIndex((fo) => fo.id === food.id) === -1) {
-            setCartItem((prevState) => [...prevState, food]);
-        } else {
-            setCartItem((prevState) => {
-                return prevState.map((item) => {
-                    return item.id === food.id ? { ...item, quantity: item.quantity + 1 } : item
-                })
-            });
-        }
-        handleClick();
-    };
+  const totalPrice = price * quantity;
 
-    return (
-        <div className="bg-color3 flex flex-col w-full items-center rounded-3xl">
-            <div>
-                <Link href={`/detail?id=${id}`}>
-                    <h1 className="font-pacifico text-3xl text-color1 p-1">{name}</h1>
-                </Link>
-            </div>
+  return (
+    <div className="bg-color3 flex flex-col w-full items-center rounded-3xl">
+      <div>
+        <Link href={`/detail?id=${id}`}>
+          <h1 className="font-pacifico text-3xl text-color1 p-1">{name}</h1>
+        </Link>
+      </div>
 
-            <div className='flex flex-row w-full rounded-3x1 px-4 justify-around'>
-
-                <div className="w-60 h-32">
-                    <img src={image} alt={name} className="w-full h-full object-cover rounded-3xl" />
-                </div>
-
-                <div className='flex flex-col items-center justify-around'>
-                    <div onClick={addItemToCart} className='cursor-pointer'>
-
-                        <PlusCircleIcon className="h-10 w-10 bg-#E96479 inset-13 top-13 left-89.92 right-4.9 bottom-68 text-color2" />
-                    </div>
-                    <p className="text-pacifico text-2xl text-color1">${price}</p>
-                </div>
-            </div>
-
-            <div>
-                <p className="px-4 py-2 text-color2 font-josefin text-base">{description}</p>
-            </div>
+      <div className='flex flex-row w-full rounded-3x1 px-4 justify-around'>
+        <div className="w-60 h-32">
+          <img src={image} alt={name} className="w-full h-full object-cover rounded-3xl" />
         </div>
-    );
+        <div className='flex flex-col items-center justify-around'>
+          <div className='flex items-center'>
+            <button
+              onClick={handleDecrement}
+              className='px-2 py-1 rounded-l-md border text-color2 border-color2 focus:outline-none'
+            >
+              -
+            </button>
+            <input
+              type="number"
+              value={quantity}
+              onChange={(e) => setQuantity(parseInt(e.target.value))}
+              className="w-12 h-8 px-2 py-1 text-center rounded-md border border-color2 focus:outline-none font-manrope text-color2 font-bold"
+            />
+            <button
+              onClick={handleIncrement}
+              className='px-2 py-1 rounded-r-md border text-color2 border-color2 focus:outline-none'
+            >
+              +
+            </button>
+          </div>
+          <div onClick={addItemToCart} className='cursor-pointer'>
+            <PlusCircleIcon className="h-10 w-10 bg-#E96479 inset-13 top-13 left-89.92 right-4.9 bottom-68 text-color2" />
+          </div>
+          <p className="text-pacifico text-2xl text-color1">${totalPrice}</p>
+        </div>
+      </div>
+
+      <div>
+        <p className="px-4 py-2 text-color2 font-josefin text-base">{description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,33 +1,71 @@
-import ShoppingCart from '@/components/ShoppingCart/ShoppingCart';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 import { cartState } from '../../../atoms/cartState';
 import CartList from '@/components/CartList/CartList';
 import Layaout from '@/components/Layaout/Layaout';
 
 const Cart = () => {
-    const [ cartItem, setCartItem ] = useRecoilState(cartState);
+  const [cartItem, setCartItem] = useRecoilState(cartState);
+  const [total, setTotal] = useState(0);
 
-    const totalPrice = () => {
-      let total = 0;
-      cartItem.forEach((item) => total += (Math.ceil(item.price) * item.quantity));
-      return total;
-    };
+  const calculateTotalPrice = () => {
+    let newTotal = 0;
+    cartItem.forEach((item) => (newTotal += Math.ceil(item.price) * item.quantity));
+    return newTotal;
+  };
 
+  const updateTotalPrice = () => {
+    const newTotal = calculateTotalPrice();
+    setTotal(newTotal);
+  };
+
+  const handleQuantityChange = (itemId, newQuantity) => {
+    const updatedCartItems = cartItem.map((item) =>
+      item.id === itemId ? { ...item, quantity: newQuantity } : item
+    );
+    setCartItem(updatedCartItems);
+  };
+
+  const removeItem = (itemId) => {
+    const updatedCartItems = cartItem.filter((item) => item.id !== itemId);
+    setCartItem(updatedCartItems);
+  };
+
+  useEffect(() => {
+    updateTotalPrice();
+  }, [cartItem]);
 
   return (
     <Layaout>
-    <div>
+      <div>
         <div className='container mx-auto'>
-            {cartItem.length <= 0 ? <h1 className='text-center text-4xl mt-32'>Tu carrito esta vacio</h1> : cartItem.map((item) => <CartList key={item.id} data={item}/>)}
+          {cartItem.length <= 0 ? (
+            <h1 className='text-center text-4xl mt-32'>Tu carrito está vacío</h1>
+          ) : (
+            cartItem.map((item) => (
+              <CartList
+                key={item.id}
+                food={item}
+                handleQuantityChange={handleQuantityChange}
+                removeItem={removeItem}
+              />
+            ))
+          )}
 
-            {cartItem.length > 0 && (<div className='max-w-[800px] mx-auto mt-4'>
-              <h2 className='text-right text-3xl font-bold'>Total a pagar: ${totalPrice()}</h2>
-              <button className='text-right bg-red-600 text-white py-4 px-12 mt-4 block mx-auto hover:bg-red-800'>Pagar</button>
-            </div>)} 
+          {cartItem.length > 0 && (
+            <div className='max-w-[800px] mx-auto mt-4'>
+              <div className='flex flex-row items-center'>
+                <h2 className='text-center text-2xl font-bold font-manrope'>
+                  Total a pagar: ${total}
+                </h2>
+                <button className='text-right bg-red-600 text-white py-4 px-12 mt-4 block mx-auto hover:bg-red-800 rounded mb-2'>
+                  Pagar
+                </button>
+              </div>
+            </div>
+          )}
         </div>
-    </div>
-
+      </div>
     </Layaout>
   );
 };
