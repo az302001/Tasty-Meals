c50566d02fbca89dc3319ce0450d37ccf616bda4
Erick0909sc <erick_osoret@outlook.com>
2023-06-16 12:00:49 -0500

    correcines

--- src/pages/api/Auth/login.js
+++ src/pages/api/Auth/login.js
@@ -3,57 +3,66 @@ import { signToken } from "@/utils/jwt.handle";
 import bcrypt from "bcrypt";
 
 export default async function handler(req, res) {
-    const { method, body } = req;
+  const { method, body } = req;
 
-    switch (method) {
-        case 'POST':
-            const { email, password } = body;
+  switch (method) {
+    case "POST":
+      const { email, password } = body;
 
-            // Validar si los campos están vacíos
-            if (!email || !password) {
-                res
-                    .status(400)
-                    .json({
-                        error: "Debes ingresar un correo electrónico y una contraseña",
-                    });
-                return;
+      // Validar si los campos están vacíos
+      if (!email || !password) {
+        res.status(400).json({
+          error: "Debes ingresar un correo electrónico y una contraseña",
+        });
+        return;
+      }
+
+      try {
+        // Verificar si el usuario ya existe en la base de datos
+        const existingUser = await prisma.user.findUnique({
+          where: { email },
+        });
+        if (!existingUser) {
+          // El correo electrónico no está registrado
+          res
+            .status(400)
+            .json({ error: "El correo electrónico no está registrado" });
+          return;
+        }
+
+        if (existingUser) {
+          // El usuario ya existe, verificar la contraseña
+          const dbPassword = existingUser.password;
+          const match = await bcrypt.compare(password, dbPassword);
+
+          const token = await signToken({ email });
+
+          if (match) {
+            // Las credenciales son válidas, redirigir al dashboard si el correo electrónico es "admin@gmail.com"
+            if (email === "tastymeals.0101@gmail.com") {
+              res
+                .status(200)
+                .json({ dashboardUrl: "/dashboard", token, email });
+              return;
             }
 
-            try {
-                // Verificar si el usuario ya existe en la base de datos
-                const existingUser = await prisma.user.findUnique({
-                    where: { email },
-                });
+            res
+              .status(200)
+              .json({ message: "Inicio de sesión exitoso", token, email });
+          } else {
+            // Contraseña incorrecta
+            res.status(400).json({ error: "Contraseña incorrecta" });
+          }
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Ocurrió un error en el servidor" });
+      }
 
-                if (existingUser) {
-                    // El usuario ya existe, verificar la contraseña
-                    const dbPassword = existingUser.password;
-                    const match = await bcrypt.compare(password, dbPassword);
+      break;
 
-                    const token = await signToken({ email })
-
-                    if (match) {
-                        // Las credenciales son válidas, redirigir al dashboard si el correo electrónico es "admin@gmail.com"
-                        if (email === 'admin@gmail.com') {
-                            res.status(200).json({ dashboardUrl: '/dashboard', token, email });
-                            return;
-                        }
-
-                        res.status(200).json({ message: 'Inicio de sesión exitoso', token, email });
-                    } else {
-                        // Contraseña incorrecta
-                        res.status(400).json({ error: 'Contraseña incorrecta' });
-                    }
-                }
-            } catch (error) {
-                console.error(error);
-                res.status(500).json({ error: 'Ocurrió un error en el servidor' });
-            }
-
-            break;
-
-        default:
-            res.status(405).json({ error: 'Método no permitido' });
-            break;
-    }
-};
+    default:
+      res.status(405).json({ error: "Método no permitido" });
+      break;
+  }
+}
