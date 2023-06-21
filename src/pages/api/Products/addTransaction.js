393b257eac98d00d86e1dfcd52016ce87662de70
TrilloJulioFrancisco <juliotrillobaron@gmail.com>
2023-06-21 17:41:34 -0300

    acomodé un break

--- src/pages/api/Products/addTransaction.js
+++ src/pages/api/Products/addTransaction.js
@@ -76,13 +76,13 @@ export default async function handler(req, res) {
           .status(200)
           .json({ mensaje: "Estado de transacción actualizado correctamente" });
 
-          break;
+          
       } catch (error) {
         res
           .status(500)
           .json({ error: "Error al actualizar el estado de la transacción" });
       }
-
+      break;
     default:
       res.status(405).json({ error: "Método no permitido" });
       break;
