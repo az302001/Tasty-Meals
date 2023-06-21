3b503a01541b571dd9776cde4a5364c0e7476100
Bruno Santimaria <brun0.23@outlook.com>
2023-06-21 17:42:43 -0300

    arreglado varios estilos

--- src/components/Layaout/Footer/Footer.jsx
+++ src/components/Layaout/Footer/Footer.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import Instagram from "@/assets/instagram.svg";
 import Facebook from "@/assets/facebook.svg";
 import Whatsapp from "@/assets/whatsapp.svg";
+import Link from "next/link";
 
 const Footer = () => {
+  const handleLinkClick = (url) => {
+    window.open(url, "_blank");
+  };
+
   return (
-    <div className='flex justify-evenly w-full bottom-0 left-0 bg-color3 h-full p-4 border-t-2 border-color1'>
-      <div name="facebook-logo" className="w-8 h-8">
-        <Image src={Facebook} alt={"facebook"}></Image>
-      </div>
-      <div name="instagram-logo" className="w-8 h-8">
-        <Image src={Instagram} alt={"instagram"}></Image>
-      </div>
-      <div name="whatsapp-logo" className="w-8 h-8">
-        <Image src={Whatsapp} alt={"whatsapp"}></Image>
-      </div>
+    <div className="flex justify-evenly w-full bottom-0 left-0 bg-color3 h-full p-4 border-t-2 border-color1">
+      <section className="w-3/4 flex justify-evenly">
+        <div
+          name="facebook-logo"
+          className="w-8 h-8 cursor-pointer"
+          onClick={() => handleLinkClick("https://facebook.com")}
+        >
+          <Image src={Facebook} alt="facebook" height={35} width={35} />
+        </div>
+        <div
+          name="instagram-logo"
+          className="w-8 h-8 cursor-pointer"
+          onClick={() => handleLinkClick("https://instagram.com")}
+        >
+          <Image src={Instagram} alt="instagram" height={35} width={35} />
+        </div>
+        <div
+          name="whatsapp-logo"
+          className="w-8 h-8 cursor-pointer"
+          onClick={() => handleLinkClick("https://whatsapp.com")}
+        >
+          <Image src={Whatsapp} alt="whatsapp" height={35} width={35} />
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
